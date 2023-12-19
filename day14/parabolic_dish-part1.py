b1e2545eefb474b17f3b5d78239a6721c70b3328
sachinnsit06 <101973239+sachinnsit06@users.noreply.github.com>
2023-12-19 22:05:45 +0100

    Update parabolic_dish-part1.py
    added elapsed time:
    
    You are one gold star closer to restoring snow operations. [Continue to Part Two]

--- day14/parabolic_dish-part1.py
+++ day14/parabolic_dish-part1.py
@@ -1,3 +1,5 @@
+import time
+
 def sort_string_with_O_and_keep_hash(string):
     sortedList =[]
     for i in string.split("#"):
@@ -7,8 +9,8 @@ def sort_string_with_O_and_keep_hash(string):
     rtrString = "".join(sortedList)
     return rtrString[:len(string)]
 
+st = time.time()
 inputData = open(r"C:\Users\user\python\adventOfCode\adventOfCode\day14\input.txt", 'r').read().splitlines()
-
 #tanspose --> columns become row and it becomes easy to sort
 rotatedData = list(map("".join, zip(*inputData)))
 
@@ -25,7 +27,9 @@ load = 0
 for eachRow in transposedData:
     no_of_rocks = eachRow.count('O')
     load = load + no_of_rocks*distance_north_south
-    print("Load at ", distance_north_south, " is ", load)
     distance_north_south = distance_north_south -1 
 
 print("Final Load is", load)
+et = time.time()
+elapsed = et - st
+print('Execution time:', elapsed, 'seconds')
