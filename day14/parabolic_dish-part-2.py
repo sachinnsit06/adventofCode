a809154e79d70b6eb5f9b42e982b2280734f6657
sachinnsit06 <101973239+sachinnsit06@users.noreply.github.com>
2023-12-20 00:19:44 +0100

    Update parabolic_dish-part-2.py
    PS C:\Users\user\python\adventOfCode\adventOfCode\day14> & C:/Users/user/AppData/Local/Programs/Python/Python310/python.exe c:/Users/user/python/adventOfCode/adventOfCode/day14/parabolic_dish-part-2.py
    This state of dish has been seen before spinCyle : 155
    Final Load is 99166
    Execution time: 1.2397029399871826 seconds
    PS C:\Users\user\python\adventOfCode\adventOfCode\day14>

--- day14/parabolic_dish-part-2.py
+++ day14/parabolic_dish-part-2.py
@@ -3,7 +3,7 @@ import time
 def sortString(string,direction):
     sortedList =[]
     for i in string.split("#"):
-        if direction == "North" or direction == "East":
+        if direction == "North" or direction == "West":
             sortSubString = sorted(i)[::-1]
         else:
             sortSubString = sorted(i)
@@ -37,24 +37,29 @@ def tiltDish(inputData,direction):
 
 #start Here
 st = time.time()
-
+tiltedDishOccurence = {}
 inputData = open(r"C:\Users\user\python\adventOfCode\adventOfCode\day14\input.txt", 'r').read().splitlines()
-for run in range(0,1000000000):
-    for direction in ["North", "East", "South", "West"]:
-        #print("move towards ", direction)
+for run in range(1,1000000000):
+    for direction in ["North", "West",  "South", "East"]:
         afterMove = tiltDish(inputData,direction)
         inputData = afterMove
+    
+    occurence = ''.join(inputData)      
+    if tiltedDishOccurence.get(occurence, 0) == 1:
+        print("This state of dish has been seen before", "spinCyle :", run)
+        break
+    else:
+        tiltedDishOccurence[occurence] = 1
+            
 
 distance_north_south = len(inputData[0])
 load = 0
 for eachRow in inputData:
     no_of_rocks = eachRow.count('O')
     load = load + no_of_rocks*distance_north_south
-    #print("Load at ", distance_north_south, " is ", load)
     distance_north_south = distance_north_south -1 
 print("Final Load is", load)
 
 et = time.time()
 elapsed = et - st
 print('Execution time:', elapsed, 'seconds')
-
